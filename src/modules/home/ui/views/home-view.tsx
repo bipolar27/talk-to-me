a68a75b8da39eb2760b079d6c9b40648df39b25a
Souradip <souro27dip@gmail.com>
2025-10-26 23:38:53 +0530

    8.trpc_setup

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,33 +1,18 @@
 "use client";
 
-import { authClient } from "@/lib/auth-client"; //import the auth client
+import { authClient } from "@/lib/auth-client"; 
 import { Button } from "@/components/ui/button";
-import {useRouter} from "next/navigation";
+import { useRouter } from "next/navigation";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
-export const HomeView=() =>{
-    const router=useRouter();
-   const {data :session}=authClient.useSession();
+export const HomeView = () => {
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.hello.queryOptions({ text: "Souradip" }));
 
-
-  if(!session){
-    return(
-      <p>Loading.....</p>
-    )
-  }
-   return(
-     <div className = "flex flex-col p-4 gap-4-y">
-        <p>logged in as {session.user.name}</p>
-        <Button onClick={()=>authClient.signOut({
-            fetchOptions:{
-                onSuccess:()=>router.push("/sign-in")
-
-            }
-        })
-    }>
-          SignOut
-        </Button>
-      </div>
-    )
-  }
-
- 
+  return (
+    <div className="flex flex-col p-4 gap-y-4">
+      {data?.greeting}
+    </div>
+  );
+};
