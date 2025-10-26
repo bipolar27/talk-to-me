bb1b14270f0efbd5bd240538444b80fd1e619f68
Sanchali Das <dassanchali219@gmail.com>
2025-10-26 13:55:15 +0530

    7.dashboard-navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,7 @@
 import {SidebarProvider} from "@/components/ui/sidebar";
+import { DashboardNavbar } from "@/modules/dashboard/ui/components/dashboard-navbar";
 import { DashboardSidebar } from "@/modules/dashboard/ui/components/dashboard-sidebar";
+
 
 
 
@@ -12,6 +14,7 @@ const Layout =({children}:Props)=>{
         <SidebarProvider>
             <DashboardSidebar/>
             <main className="flex flex-col h-screen w-screen bg-muted">
+                <DashboardNavbar/>
                 {children}
             </main>
         </SidebarProvider>
