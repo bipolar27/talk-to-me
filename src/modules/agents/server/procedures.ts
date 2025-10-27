8795a512e0b9a0e8fd44b52cf01a4e0560e91ed5
Sanchali Das <dassanchali219@gmail.com>
2025-10-27 20:57:17 +0530

    12.agent-data-table

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,5 +1,5 @@
 import { db } from "@/db";
-import {eq} from "drizzle-orm";
+import { eq, getTableColumns,sql } from "drizzle-orm";
 import { agents } from "@/db/schema";
 import { createTRPCRouter,baseProcedure, protectedProcedure } from "@/trpc/init";
 import { TRPCError } from "@trpc/server";
@@ -10,7 +10,12 @@ import z from "zod";
 export const agentsRouter=createTRPCRouter({
     getOne:protectedProcedure.input(z.object({id:z.string()})).query(async({input})=>{
         const [existingAgent]= await db
-                            .select()
+                            .select({
+                                //todo change to actual count
+                                meetingCount:sql<number>`5`,
+                                ...getTableColumns(agents),
+                               
+                            })
                             .from(agents)
                             .where(eq(agents.id, input.id))
         return existingAgent;
--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -1,39 +1,45 @@
-"use client"
+"use client";
 
-import { ErrorState } from "@/components/error-state";
+import { EmptyState } from "@/components/empty-state";
 import { LoadingState } from "@/components/loading-state";
 import { Button } from "@/components/ui/button";
-import { useTRPC } from "@/trpc/client"
-import { useQuery, useSuspenseQuery } from "@tanstack/react-query"
-import {ResponsiveDialog} from "@/components/responsive-dialog"
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { ResponsiveDialog } from "@/components/responsive-dialog";
+import { DataTable } from "../components/data-table";
+import { columns } from "../components/columns";
 
-export const AgentsView=()=>{
-    const trpc = useTRPC();
-    const {data} = useSuspenseQuery(trpc.agents.getMany.queryOptions());
+export const AgentsView = () => {
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
-    return (
-        <div>
-            
-            {JSON.stringify(data,null,2)}
-        </div>
-    )
-}
-
-export const AgentsViewLoading =()=>{
-    return(
-        <LoadingState   
-            title = "Loading Agents"
-            description="This may take a few seconds"
+  return (
+    <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+      <DataTable data={data} columns={columns} />
+      {data.length===0 &&(
+        <EmptyState
+            title="create your first agent"
+            description="Create an agent to  join your meetings.Each agent will follow your instructions and can interact wigth participants during the call. "
         />
-    )
-}
+      )}
+    </div>
+  );
+};
 
+export const AgentsViewLoading = () => {
+  return (
+    <LoadingState
+      title="Loading Agents"
+      description="This may take a few seconds"
+    />
+  );
+};
 
-export const AgentsViewError =()=>{
-    return(
-        <ErrorState   
-            title = "Error Loading Agents"
-            description="SOmething went wrong!"
-        />
-    )
-}
+export const AgentsViewError = () => {
+  return (
+    <EmptyState
+      title="Error Loading Agents"
+      description="Something went wrong!"
+    />
+  );
+};
