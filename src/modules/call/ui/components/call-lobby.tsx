9a2d4de17bc46384d492da08eae831b1dfb0f20d
Sanchali Das <dassanchali219@gmail.com>
2025-10-29 16:20:29 +0530

    22.1.Fixed-Avatar-errror

--- src/modules/call/ui/components/call-lobby.tsx
+++ src/modules/call/ui/components/call-lobby.tsx
@@ -13,6 +13,8 @@ import {
 import { authClient } from "@/lib/auth-client";
 import { generateAvatarUri } from "@/lib/avatar";
 import {Button } from "@/components/ui/button";
+import "@stream-io/video-react-sdk/dist/css/styles.css";
+
 
 interface Props{
     onJoin:()=> void;
